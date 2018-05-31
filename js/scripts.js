5a8110f6794f730e639fdf621a5adcf82a9dbc87
Annie Shin <annieyshin@gmail.com>
2018-05-30 22:38:48 -0700

    add Js script and additional HTML text

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,27 @@
-// var add = function(number1, number2) {
-//   return number1 + number2;
-// };
-//
-// var subtract = function(number1, number2) {
-//   return number1 - number2;
-// };
-//
-// var multiply = function(number1, number2) {
-//   return number1 * number2;
-// };
-//
-// var divide = function(number1, number2) {
-//   return number1 / number2;
-// };
-//
-// var number1 = parseInt(prompt("Enter a number:"));
-// var number2 = parseInt(prompt("Enter another number:"));
-//
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+// User Interface or Front end logic
+$(document).ready(function(){
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+alert(add(number1, number2));
+
+});
+
 // var result = divide(number1, number2);
 // alert(result);
 
@@ -50,11 +55,11 @@
 
 // Cups to Gallons
 
-var gallons = function(cups) {
-  return cups;
-};
-
-var cups = parseInt(prompt("How many cups do you have?"));
-
-var result = ("You have a total of " + cups/16 + " gallons.");
-alert(result);
+// var gallons = function(cups) {
+//   return cups;
+// };
+//
+// var cups = parseInt(prompt("How many cups do you have?"));
+//
+// var result = ("You have a total of " + cups/16 + " gallons.");
+// alert(result);
